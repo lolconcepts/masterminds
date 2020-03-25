c9268cc3922a92b4ce519eeeed6d0d60b56ff1fa
Bryan Stewart <bstewart@lolconcepts.com>
2020-03-25 12:47:23 -0400

    added APP_URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'lolconcepts.com' }
+  config.action_mailer.default_url_options = { :host => ENV['APP_URL'] }
 
   config.action_mailer.smtp_settings = {
     :address              => "mail.lolconcepts.com",
