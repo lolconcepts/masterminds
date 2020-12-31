695fe788ed4517cd7bbcc7a1f095245a606717c6
Bryan Stewart <bstewart@lolconcepts.com>
2020-12-31 08:06:43 -0500

    changed dashboard teacher, title and added env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	protected
+	@title = ENV["appname"] || "Mentorship"
+    protected
 	def configure_permitted_parameters
- 	attributes = [:first_name, :last_name,:carrier_id,:mobile,:smsok]
-    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
-    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
- end
+ 	  attributes = [:first_name, :last_name,:carrier_id,:mobile,:smsok]
+        devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+        devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+    end
 	def after_sign_in_path_for(resource)
 		if User.count == 1
  			#Make Admin
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+  	@teacher = User.first.first_name || ""
   	@entries = Entry.all.where(:user_id => current_user.id)
   end
 end
