58c741e5bd0d41d725a5b092d2bfcd8bc62d9804
Bryan Stewart <bstewart@lolconcepts.com>
2021-04-21 14:45:21 -0400

    Fixed entries review

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,14 @@ class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :reason
   has_one_attached :evidence
+
+  def status
+  	@message = ""
+  	if self.reviewed
+  		@message += "Reviewed "
+  	end
+  	if self.accepted
+  		@message += "| Accepted"
+  	end
+  end
 end
