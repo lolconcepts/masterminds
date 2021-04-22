ccca0ae032a92bbfd40023153dddf4eab71faeef
Bryan Stewart <bstewart@lolconcepts.com>
2021-04-22 14:42:55 -0400

    added session count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,7 @@ module ApplicationHelper
 	def statusbar(current, goal)
 		return "#{(current.to_f / goal.to_f) * 100}"
 	end
+	def sessioncount
+		return Entry.where(user_id: current_user.id).count
+	end
 end
